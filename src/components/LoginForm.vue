e5ef19bf9c66b11dbc7976687690f63799920349
Dmytro <savinov.tim@gmail.com>
2023-05-03 23:56:02 +0530

    [TASK] styles restructure

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container">
       <section id="login">
-        <form action="">
+        <form>
           <h1>Login Form</h1>
           <div class="icon-input">
             <div class="user-icon"></div> 
-            <input type="text" placeholder="Username" id="username" />
+            <input type="text" id="username" placeholder="Username" v-model="email" />
           </div>
           <div class="icon-input">
             <div class="lock-icon"></div> 
-            <input type="password" placeholder="Password" id="password" />
+            <input type="password" id="password" placeholder="Password"  v-model="password"/>
           </div>
           <div>
-            <input type="submit" value="sign in" />
+            <input type="submit" value="sign in" @submit="login"/>
           </div>
           <div class="forgot-password">
             <span href="#">Not a member?</span>
@@ -28,6 +28,19 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'LoginForm',
+
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    login() {
+
+    }
+  }
   
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/scss/main.scss'
+import './assets/scss/_main.scss'
 
 createApp(App).mount('#app')
